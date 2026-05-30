ff7c08549235d178515b9a6e47bd6461dd05abd6
shrabon10 <omarfaruksrabon@gmail.com>
2026-05-30 20:21:23 +0600

    Server Connected Succesfully

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
